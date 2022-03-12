cba46a185c5e880edbce3205a126253829aa408b
TOLGA <exbert@exbert.com>
2022-03-13 00:53:12 +0100

    finishLine

--- hesapla.js
+++ hesapla.js
@@ -1,3 +1,10 @@
-const pi = Math.PI
+const pi = Math.PI;
 const arguments = process.argv.slice(2);
 
+function alanHesapla(rNumber) {
+  console.log(
+    `Yarıçapı (${rNumber}) olan dairenin alanı: ${pi * rNumber * rNumber}`
+  );
+}
+
+alanHesapla(arguments[0] * 1);
